731172fa1af49006e16e37752937d092b4e1a933
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-03-23 11:51:57 +0300

    Update Main.java

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         Shape[] shapes = null;
-
+  
         /*
          * TODO: Выполнить действия над массивом 'shapes'
          *
