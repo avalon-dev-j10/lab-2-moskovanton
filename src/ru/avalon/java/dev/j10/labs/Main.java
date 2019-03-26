2a0c687acb0622eab66332b37dd693efa42cd051
moskovanton <48486058+moskovanton@users.noreply.github.com>
2019-03-26 11:50:08 +0300

    Update Main.java

--- src/ru/avalon/java/dev/j10/labs/Main.java
+++ src/ru/avalon/java/dev/j10/labs/Main.java
@@ -3,6 +3,15 @@ package ru.avalon.java.dev.j10.labs;
 import ru.avalon.java.dev.j10.labs.shapes.*;
 
 public class Main {
+    
+    static void maxArea(Shape shapes[]) {
+        System.out.println("------------------------------------------------");
+        int maxIndex = 0;
+        for(int i = 0; i < shapes.length - 1; i++){
+            if(shapes[maxIndex].getArea() < shapes[i+1].getArea()) maxIndex = i+1;
+        }
+        System.out.println(maxIndex+1 + ". " + shapes[maxIndex].getType() + ", MAX S = " + shapes[maxIndex].getArea());  
+    }
 
     public static void main(String[] args) {
         Shape[] shapes = new Shape[10];
@@ -24,25 +33,17 @@ public class Main {
         shapes[1] = new Rectangle(10, 6);
         shapes[2] = new Triangle(3, 4, 5, 3);
         shapes[3] = new Circle(7);
-        shapes[4] = new Rectangle(35, 11);
+        shapes[4] = new Rectangle(35, 11.4f);
         shapes[5] = new Triangle(4, 5, 2, 3);
         shapes[6] = new Circle(2.3f);
         shapes[7] = new Rectangle(8, 4);
         shapes[8] = new Triangle(11, 1, 2, 2);
         shapes[9] = new Circle(8.1f);
         
-        int maxIndex = 0;
-        
         for(int i = 0; i < shapes.length; i++){
             System.out.println(i+1 + ". " + shapes[i].getType() + ", S = " + shapes[i].getArea());
         }
         
-        System.out.println("------------------------------------------------");
-        
-        for(int i = 0; i < shapes.length - 1; i++){
-            if(shapes[maxIndex].getArea() < shapes[i+1].getArea()) maxIndex = i+1;
-        }
-        
-        System.out.println(maxIndex+1 + ". " + shapes[maxIndex].getType() + ", MAX S = " + shapes[maxIndex].getArea());
+        maxArea(shapes);
     }
 }
